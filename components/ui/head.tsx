d5c0c38249a4032802b13a9da6132af9eb66df68
socrateNz <sssnzogning0@gmail.com>
2025-08-06 05:44:00 +0100

    font

--- components/ui/head.tsx
+++ components/ui/head.tsx
@@ -8,7 +8,7 @@ interface HeadProps {
 const Head = ({ title, description }: HeadProps) => {
     return (
         title && <div className='w-full h-fit bg-[#34773D] flex flex-col gap-7 items-center justify-center text-white py-20'>
-            <h1 className='text-5xl font-bold max-w-[1106px] text-center'>{title || "IUJP2 - Institut Universitaire Jean-Paul II"}</h1>
+            <h1 className='text-5xl font-serif font-bold max-w-[1106px] text-center'>{title || "IUJP2 - Institut Universitaire Jean-Paul II"}</h1>
             {/* <p className='mt-2 text-lg max-w-[1106px] text-center'>{description || "Bienvenue à l'Institut Universitaire Jean-Paul II, votre destination pour une éducation de qualité."}</p> */}
         </div>
     )
